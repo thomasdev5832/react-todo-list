3db876cc4ad45913886d7753818fbdc2d6cc1680
thomasdev5832 <eugabrielthome@gmail.com>
2025-09-18 13:58:36 -0300

    improve layout

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const Filter = ({ filter, setFilter, setSort }) => {
   return (
     <div className="filter">
@@ -22,5 +24,11 @@ const Filter = ({ filter, setFilter, setSort }) => {
     </div>
   )
 }
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  setFilter: PropTypes.func.isRequired,
+  setSort: PropTypes.func.isRequired,
+};
 
 export default Filter
